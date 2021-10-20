f91bee2ec67476a9a8c7ad66fac14bf53f9197a3
chaitanyavasu123 <92853761+chaitanyavasu123@users.noreply.github.com>
2021-10-20 18:55:13 +0530

    CD LAB RECORD

--- lab 3/3.2/lex.yy.c
+++ lab 3/3.2/lex.yy.c
@@ -1510,7 +1510,7 @@ int main()
 int yywrap(){}
 int main()
 {
-    printf("Lab Work performed by SAVITA NANDAN 2019UGCS055R\n");
+    printf("THIS PROGRAMME IS FOR COUNTING OF VOWELS AND CONSONENTS\n");
     yylex();
    return 0;
 }
